199795ca693e9cb8c4687820aa05d9cc820236a7
Justin Sherburne <sherburj@os2.engr.oregonstate.edu>
2017-10-28 18:52:07 -0700

    added the Kconfig for modification

--- hw3/sstf-iosched.c
+++ hw3/sstf-iosched.c
@@ -1,5 +1,5 @@
 /*
- * elevator clook
+ * elevator sstf
  */
 #include <linux/blkdev.h>
 #include <linux/elevator.h>
@@ -8,100 +8,79 @@
 #include <linux/slab.h>
 #include <linux/init.h>
 
-//int diskhead = -1;
+int diskhead = -1;
 
-struct clook_data {
+struct sstf_data {
 	struct list_head queue;
 };
 
-static void clook_merged_requests(struct request_queue *q, struct request *rq,
+static void sstf_merged_requests(struct request_queue *q, struct request *rq,
 				 struct request *next)
 {
 	list_del_init(&next->queuelist);
-	//elv_dispatch_sort(q, next);
+	elv_dispatch_sort(q, next);
 }
 
-static int clook_dispatch(struct request_queue *q, int force)
+static int sstf_dispatch(struct request_queue *q, int force)
 {
-	struct clook_data *nd = q->elevator->elevator_data;
-	//struct request *rq;
+	struct sstf_data *nd = q->elevator->elevator_data;
+	struct request *rq;
 	char direction;
 
-	//rq = list_first_entry_or_null(&nd->queue, struct request, queuelist);
-	if (!list_empty(&nd->queue)/*rq*/) {
+	rq = list_first_entry_or_null(&nd->queue, struct request, queuelist);
+	if (!list_empty(&nd->queue)rq) {
 		struct request *rq;
-		rq = list_entry(nd->queue.next, struct request, queuelist); //get rid of this
 		list_del_init(&rq->queuelist);
 		elv_dispatch_sort(q, rq);
 				
-		//diskhead = blk_rq_pos(rq);
+		diskhead = blk_rq_pos(rq);
 		
-		//if(rq_data_dir(rq) == READ)
-		//	direction = 'R';
-		//else	
-		//	direction = 'W';
-		//printk("[CLOOK] dsp %c %lu\n", direction, blk_rq_pos(rq));
+		if(rq_data_dir(rq) == READ)
+			direction = 'R';
+		else	
+			direction = 'W';
+		printk("[sstf] dsp %c %lu\n", direction, blk_rq_pos(rq));
 		
 		return 1;
 	}
 	return 0;
 }
 
-static void clook_add_request(struct request_queue *q, struct request *rq)
+static void sstf_add_request(struct request_queue *q, struct request *rq)
 {
-	struct clook_data *nd = q->elevator->elevator_data;
-	//struck list_head *cur = NULL;
-	//char direction;
+	struct sstf_data *nd = q->elevator->elevator_data;
+	struck list_head *cur = NULL;
+	char direction;
 	
-	list_add_tail(&rq->queuelist, &nd->queue); //get rid of me
-	
-	/*list_for_each(cur, &nd->queue)
-	{
-		// Set cur to cur list_entry
+	list_for_each(cur, &nd->queue){
 		struct request *c = list_entry(cur, struct request, queuelist);
-		
-		// If cur position is greater than diskhead...
-		if (blk_rq_pos(rq) > diskhead)
-		{
-			// If the cur position < diskhead OR rq position < cur position... break and add to cur position
-			if(blk_rq_pos(c) < diskhead || blk_rq_pos(rq) < blk_rq_pos(c))
-				break;
-			
-		} else {
-			// cur pos less than disk head
-			// If the cur position < diskhead AND rq position < cur position... break and add to cur position
-			if(blk_rq_pos(c) < diskhead && blk_rq_pos(rq) < blk_rq_pos(c))
-				break;
-		}
 	}
 	
-	// Add cur request to request queue
 	list_add_tail(&rq->queuelist, cur);
 	
-	// Check direction for read or write
 	if(rq_data_dir(rq) == READ)
 		direction = 'R';
 	else
 		direction = 'W';
-	printk("[CLOOK] dsp %c %lu\n", direction, blk_rq_pos(rq));
-	*/
+	printk("[sstf] dsp %c %lu\n", direction, blk_rq_pos(rq));
+	
 }
 
 static struct request *
-clook_former_request(struct request_queue *q, struct request *rq)
+sstf_former_request(struct request_queue *q, struct request *rq)
 {
-	struct clook_data *nd = q->elevator->elevator_data;
+	struct sstf_data *nd = q->elevator->elevator_data;
 
 	if (rq->queuelist.prev == &nd->queue)
 		return NULL;
 	
-//	return list_prev_entry(rq, queuelist);
+	return list_prev_entry(rq, queuelist);
 	return list_entry(rq->queuelist.prev, struct request, queuelist);
 }
 
-static int clook_init_queue(struct request_queue *q, struct elevator_type *e)
+static int sstf_init_queue(struct request_queue *q, struct elevator_type *e)
 {
-	struct clook_data *nd;
+	struct sstf_data *nd;
 	struct elevator_queue *eq;
 
 	eq = elevator_alloc(q, e);
@@ -123,41 +102,41 @@ static int clook_init_queue(struct request_queue *q, struct elevator_type *e)
 	return 0;
 }
 
-static void clook_exit_queue(struct elevator_queue *e)
+static void sstf_exit_queue(struct elevator_queue *e)
 {
-	struct clook_data *nd = e->elevator_data;
+	struct sstf_data *nd = e->elevator_data;
 
 	BUG_ON(!list_empty(&nd->queue));
 	kfree(nd);
 }
 
-static struct elevator_type elevator_clook = {
+static struct elevator_type elevator_sstf = {
 	.ops = {
-		.elevator_merge_req_fn		= clook_merged_requests,
-		.elevator_dispatch_fn		= clook_dispatch,
-		.elevator_add_req_fn		= clook_add_request,
-		.elevator_former_req_fn		= clook_former_request,
-		.elevator_init_fn			= clook_init_queue,
-		.elevator_exit_fn			= clook_exit_queue,
+		.elevator_merge_req_fn		= sstf_merged_requests,
+		.elevator_dispatch_fn		= sstf_dispatch,
+		.elevator_add_req_fn		= sstf_add_request,
+		.elevator_former_req_fn		= sstf_former_request,
+		.elevator_init_fn		= sstf_init_queue,
+		.elevator_exit_fn		= sstf_exit_queue,
 	},
-	.elevator_name = "clook",
+	.elevator_name = "sstf",
 	.elevator_owner = THIS_MODULE,
 };
 
-static int __init clook_init(void)
+static int __init sstf_init(void)
 {
-	return elv_register(&elevator_clook);
+	return elv_register(&elevator_sstf);
 }
 
-static void __exit clook_exit(void)
+static void __exit sstf_exit(void)
 {
-	elv_unregister(&elevator_clook);
+	elv_unregister(&elevator_sstf);
 }
 
-module_init(clook_init);
-module_exit(clook_exit);
+module_init(sstf_init);
+module_exit(sstf_exit);
 
 
 MODULE_AUTHOR("David Baugh/Justin Sherburne");
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("CLOOK IO scheduler");
+MODULE_DESCRIPTION("sstf IO scheduler");
